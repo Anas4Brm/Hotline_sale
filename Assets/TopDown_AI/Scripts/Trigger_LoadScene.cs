3e85ef0f25b0c69d81a6540dd4a66f0571cd20a9
You SseF II <wacwacwac807@gmail.com>
2025-03-20 04:12:43 +0000

    Mettre les scenes ensembles

--- Assets/TopDown_AI/Scripts/Trigger_LoadScene.cs
+++ Assets/TopDown_AI/Scripts/Trigger_LoadScene.cs
@@ -1,20 +1,28 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.SceneManagement;
 
-public class Trigger_LoadScene : MonoBehaviour {
-	public string sceneName;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-	void OnTriggerEnter(Collider user){
-		if (user.tag == "Player") {
-			Application.LoadLevel(sceneName);
-		}
-	}
+public class Trigger_LoadNextScene : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // Vérifie si le joueur entre dans le déclencheur
+        {
+            LoadNextScene(); // Charge la scène suivante
+        }
+    }
+
+    private void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex; // Index de la scène actuelle
+        int nextSceneIndex = currentSceneIndex + 1; // Index de la scène suivante
+
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings) // Vérifie si la scène suivante existe
+        {
+            SceneManager.LoadScene(nextSceneIndex); // Charge la scène suivante
+        }
+        else
+        {
+            Debug.LogWarning("No next scene available. This is the last scene.");
+        }
+    }
 }
